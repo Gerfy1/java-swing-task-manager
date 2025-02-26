45918b0c18a4fbacfdf5133db79962a3da820752
Gerfy1 <gerfynlol@gmail.com>
2025-02-26 19:26:32 -0300

    fix: task

--- src/main/java/com/task/taskManagerSolid/controller/TaskController.java
+++ src/main/java/com/task/taskManagerSolid/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/tasks")
 public class TaskController {
 
     private TaskService taskService;
