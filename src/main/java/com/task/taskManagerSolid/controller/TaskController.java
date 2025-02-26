3d556e462679424320ebc142b04ccc137f34a58a
Gerfy1 <gerfynlol@gmail.com>
2025-02-26 18:34:27 -0300

    update: jwt + controller

---
+++ src/main/java/com/task/taskManagerSolid/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.task.taskManagerSolid.controller;
+
+import com.task.taskManagerSolid.dto.TaskDTO;
+import com.task.taskManagerSolid.entity.Task;
+import com.task.taskManagerSolid.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAll(){
+        return ResponseEntity.ok(taskService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> create(@RequestBody TaskDTO taskDTO){
+        return ResponseEntity.ok(taskService.create(taskDTO));
+    }
+}
