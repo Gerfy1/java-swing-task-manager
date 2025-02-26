0537cc1d46e5aaef688419995e3fe2a873559f22
Gerfy1 <gerfynlol@gmail.com>
2025-02-26 18:06:49 -0300

    Start study

---
+++ src/main/java/com/task/taskManagerSolid/TaskManagerSolidApplication.java
@@ -0,0 +1,13 @@
+package com.task.taskManagerSolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSolidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/taskManagerSolid/TaskManagerSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.taskManagerSolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
