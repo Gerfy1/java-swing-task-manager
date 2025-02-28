6be8a4cada151dad544ae43c5bfadead6426ad78
Gerfy1 <gerfynlol@gmail.com>
2025-02-27 23:13:22 -0300

    update: securityConfig

--- src/main/java/com/task/taskManagerSolid/security/SecurityConfig.java
+++ src/main/java/com/task/taskManagerSolid/security/SecurityConfig.java
@@ -1,8 +1,24 @@
 package com.task.taskManagerSolid.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        .requestMatches("/taks/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic();
+        return http.build();
+    }
+
 }
