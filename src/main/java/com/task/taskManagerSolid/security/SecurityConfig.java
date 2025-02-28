cce1e07318e9e1ab6ef335035020f2a44db1c29d
Gerfy1 <gerfynlol@gmail.com>
2025-02-27 23:24:53 -0300

    fix: Security

--- src/main/java/com/task/taskManagerSolid/security/SecurityConfig.java
+++ src/main/java/com/task/taskManagerSolid/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.task.taskManagerSolid.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +14,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests(auth ->
-                        .requestMatches("/taks/**").permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/taks/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
         return http.build();
     }
 
